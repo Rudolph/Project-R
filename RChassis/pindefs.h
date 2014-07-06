9ddfb71bb315dcf625bf94687b5b6f14c911d901
Del Rudolph <del@rudforce.com>
2014-07-06 09:51:26 -0700

    filling in the vars...

--- RChassis/pindefs.h
+++ RChassis/pindefs.h
@@ -1,17 +1,17 @@
 // Pin Definitions
 // left motor PWM
-#define lMotor1  3
-#define lMotor2  4
+#define lMotor1  6
+#define lMotor2  7
 
 // right motor PWM
-#define rMotor1  6
-#define rMotor2  7
+#define rMotor1  3
+#define rMotor2  4
 
 // pins for both motor drivers
 #define nFault  2  // interrupt2
 #define nSleep  5  // drive low to sleep
 
-// wheel encoders, not that we address interrupts by pin number...
+// wheel encoders
 #define lEnc  10  // interrupt0
 #define rEnc  11  // interrupt 1
 
